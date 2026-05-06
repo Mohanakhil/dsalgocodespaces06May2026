82c62a74b6be57eef8be28af68eed909ecc637f7
DENDULURI RAMA SAI MOHAN AKHIL <62695182+Mohanakhil@users.noreply.github.com>
2026-05-06 09:57:31 +0000

    1 progr done

---
+++ JavaPractice/Basics/EvenOdd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class EvenOdd{
+    public static boolean checkIfEven(int n){
+        if(n%2==0) return true;
+        else return false;
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        boolean flag=checkIfEven(n);
+        if(flag)System.out.println("its even no");
+        else System.out.println("its an odd no");
+    }
+}
